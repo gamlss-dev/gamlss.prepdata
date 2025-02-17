8b1b45581b713f76b98bf18127cddbb57e93b96f
Mikis Stasinopoulos <dmh.stasinopoulos@gmail.com>
2025-02-17 18:35:42 +0000

    data_association

--- R/data_cor.R
+++ R/data_cor.R
@@ -99,7 +99,6 @@ if (diffDim > 0)
               CC <- cor(daTa)
               CC <- base::round(x = CC, digits = digits)
 if (plot==FALSE) return(CC)
-  browser()            
 if ( diag.off) diag(CC) <- NA
 if (lower.tri.off)  CC[lower.tri(CC)]<-NA
               txt.title <- if (missing(title))
@@ -202,46 +201,6 @@ if (plot)
 high_val <- function(table, val=.90, digits=3, plot=FALSE, igraph=TRUE)
 {
   if (abs(val)>=1||abs(val)<=0) stop("val should be greater than  0 and lass than 1")
- 
- 
-  mm <- which(abs(table)>val, arr.ind=T)
-  nn <- mm[mm[,1]< mm[,2],]
-  if (is.vector(nn))
-  {
-    name1 <- colnames(table)[nn[1]]
-    name2 <- colnames(table)[nn[2]]
-    corrs <- table[nn[1],nn[2]]
-  } else
-  {
-    name1 <- colnames(table)[nn[,1]]
-    name2 <- colnames(table)[nn[,2]]
-    corrs <- table[nn]
-  }
-  M <-  cbind(name1, name2, corrs)
-  if (plot)
-  {
-    # dd <- which.Data.Corr(InfMort, r=0.5)[,c(1,2)]
-    #  network <- graph_from_data_frame(d=dd, directed=F)
-    # plot it
-    #  plot(network)
-    #  InfMort |> which.Data.Corr( r=0.5) |>  as.data.frame() |>
-    #    simpleNetwork(CC, height="100px", width="100px")
-    dd <- as.data.frame(M)
-    if (igraph) plot(igraph::graph_from_data_frame(d=dd, directed=F))
-    else{
-      p <- networkD3::simpleNetwork(dd, height="100px", width="100px")
-      print(p)
-    }
-  } else
-    return(M)
-}
-################################################################################
-################################################################################
-################################################################################
-################################################################################
-high_val <- function(table, val=.90, digits=3, plot=FALSE, igraph=TRUE)
-{
-  if (abs(val)>=1||abs(val)<=0) stop("val should be greater than  0 and lass than 1")
   
   
   mm <- which(abs(table)>val, arr.ind=T)
@@ -315,3 +274,7 @@ low_val <- function(table, val=.05, digits=3, plot=FALSE, igraph=TRUE)
   } else
     return(M)
 }
+################################################################################
+################################################################################
+################################################################################
+################################################################################
