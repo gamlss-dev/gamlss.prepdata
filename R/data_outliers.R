f68c6135ceeb2fc3af1322bc5ca2aa8ee2ad97a4
Mikis Stasinopoulos <dmh.stasinopoulos@gmail.com>
2025-05-03 21:00:07 +0300

    data_trans

--- R/data_outliers.R
+++ R/data_outliers.R
@@ -6,21 +6,21 @@
 ################################################################################
 ################################################################################ 
 # function 1
-y_outliers <- function(var, value=4, family=SHASH)
+y_outliers <- function(var, value=4, family=SHASHo)
 {
   if (!is(var,"numeric")) stop("the variable should be numeric")
   # the problem is integer is numeric so do not stop them
-  if (any(var<0))
+  if (any(var<0)) # if has negative values 
   {
-    tvar <- var 
-  } else
+    tvar <- var # do not look for power transformation 
+  } else  # if only positive values look for tran=foarmation  
   {
     par  <- x_Ptrans(var)
     tvar <- if (abs(par) < 0.001) log(var) else var^par
   }  
   z.scores <- y_zscores(tvar, family=family, plot=FALSE)
       ival <- var[which(abs(z.scores)>value)]
-  iind <- which(abs(z.scores)>value)
+      iind <- which(abs(z.scores)>value)
   names(ival) <- iind
   ival
 }
@@ -28,7 +28,7 @@ y_outliers <- function(var, value=4, family=SHASH)
 ################################################################################
 ################################################################################
 ################################################################################
-# function 3
+# function 2
 data_outliers <- function(data, 
                           value = 4,
                           min.distinct = 50, 
@@ -65,6 +65,64 @@ data_outliers <- function(data,
   }
   names(PP) <- Names       
    PP 
+}
+################################################################################
+################################################################################
+################################################################################
+################################################################################
+# functions 3 
+# this is a hidden function only used by y_outliers
+# it takes positive variables and check if they have high Jarque-Bera value
+# Note that the  Jarque-Bera  test statistic is always non-negative. 
+# If it is far from zero, it signals the data do not have a normal distribution.
+# I think the idea here is that if the variable is positive it is transformed 
+# to be make to have a less skew-kurtotic distribution 
+# then it fits the SHASH (It wa suggested by  Bob)
+# This prevent very highly observations in the right tail to be   
+# 
+x_Ptrans <- function(x, lim.trans = c(0, 1.5),  prof=FALSE,
+                     step=0.01,    bucket=FALSE)
+{
+if (length(lim.trans)!=2) stop(" the limits of  p are not set properly")
+  #  cat("*** Checking for transformation for x ***", "\n")
+  cX <- class(x)
+  if ((cX!="numeric")&(cX!="integer")) return(NA)
+# define the functions 
+  ptrans <- function(x, p) if (abs(p)<=0.001) log(x) else I(x^p)
+      fn <- function(p) momentSK(ptrans(x,p))$jarque.bera.test
+
+# if (prof) # profile dev
+#   {
+#     pp <- seq(lim.trans[1],lim.trans[2], step)
+#     pdev <- rep(0, length(pp))
+#     for (i in 1:length(pp))
+#     {
+#       pdev[i] <- fn(pp[i])
+#       #   cat(pp[i], pdev[i], "\n")
+#     }
+#     GG <- ggplot(data.frame(pdev,pp), aes(x=pp, y=pdev))+
+#       geom_point()+
+#       geom_line()
+#     #  points(pdev~pp,col="blue")
+#     par <- pp[which.min(pdev)]
+#     print(GG)
+#     cat('*** power parameters ', par,"***"," \n")
+#   } else
+#   {
+    par <- optimise(fn, lower=lim.trans[1], upper=lim.trans[2])$minimum
+    # cat('*** power parameters ', par,"***"," \n")
+  # }
+  PP <-  moment_bucket(ptrans(x,par),x, text_to_show=c("+","*"))
+  if (bucket) print(PP)
+  # 1-pchisq(momentSK(ptrans(x,par))$jarque.bera.test, 2)
+  # 
+  # 1-pchisq(momentSK(ptrans(x,1))$jarque.bera.test, 2)
+  # hist(ptrans(x,par))
+  #  hist(ptrans(x,1))
+  #  momentSK(ptrans(x,1))$jarque.bera.test
+#  cat('*** power parameters ', par,"***"," \n")
+  invisible(par)
+  
 }
 ################################################################################
 ################################################################################
---
+++ R/data_transformations.R
@@ -0,0 +1,244 @@
+################################################################################
+################################################################################
+# functions to check the data
+################################################################################
+################################################################################
+# rm(list=ls())
+# # checking for missing values
+# function available
+################################################################################
+#  NOT THIS ONE 1) data_check       : will be fixed in the end
+################################################################################   :
+#  1) xy_Ptrans  :  takes x and y and find power best labmda in power 
+#                 transformation
+#  2) data_Ptrans_plot
+#  3) datetime2datehour: from datetime to date and hour
+#  4) time2num 
+#
+################################################################################
+################################################################################
+################################################################################
+################################################################################
+# transformation functions
+################################################################################
+################################################################################
+################################################################################
+################################################################################
+# function 1
+# for find a single variable power transformation
+xy_Ptrans <- function(x, y, data = NULL,  lim.trans = c(0, 1.5), prof=FALSE,
+                     k=2,  c.crit = 0.01, step=0.1)
+{
+      cY <- class(y)
+      cX <- class(x)
+if (any( inherits(c(cY, cX),"numeric"))) return(NA)
+  ptrans <- function(x, p) if (abs(p)<=0.0001) log(x) else I(x^p)
+      fn <- function(p)  AIC(gam(y~s(I(ptrans(x,p)))), k=k)
+       
+if (prof) # profile dev
+  {
+     pp <- seq(lim.trans[1],lim.trans[2], step)
+    pdev <- rep(0, length(pp))
+    for (i in 1:length(pp))
+    {
+      pdev[i] <- fn(pp[i])
+#   cat(pp[i], pdev[i], "\n")
+    }
+    Labx <-paste0("GAIC(", "k=", k, ")")
+    plot(pdev~pp, type="l", ylab=Labx, xlab=expression(lambda))
+    points(pdev~pp,col="blue")
+    
+    par <- pp[which.min(pdev)]
+  } else
+  {
+     par <- optimise(fn, lower=lim.trans[1], upper=lim.trans[2])$minimum
+  }
+    cat('*** power parameters ', par,"***"," \n")      
+  invisible(par)
+}
+################################################################################
+################################################################################
+################################################################################
+################################################################################
+# function 2
+data_Ptrans_plot <- function(data, response,
+                             hist.col = "black",
+                            hist.fill = "white",
+                            dens.fill = "#FF6666",
+                                alpha = 0.2,
+                                 nrow = NULL,
+                                 ncol = NULL,
+                       plots.per.page = 9,
+                           one.by.one = FALSE,
+                            title,...)
+{
+  daTa <- mgcv <- NULL
+if (is(data, "list"))
+  stop("the data is list  the function needs a data.frame")
+if (is(data, "table"))
+  stop("the data is a table the function needs a data.frame")
+  if (is(data, "matrix"))    data <- as.data.frame(data)
+  if (is(data[1],"mts"))     data <- as.data.frame(data)
+  if (is(data, "array")) stop("the data is an array the function needs a data.frame")
+      Y <-  deparse(substitute(response))
+  if (any(!(Y %in%names(data)))) stop("the response should be in data")
+  Names <- names(data)
+  pos <- match(Y, Names)
+  daTa <- data[,-pos] # data without response
+  class_Vars <- sapply(data,function(x) class(x)[1]) 
+  daTa <- data_only_continuous(daTa) 
+  #daTA <- daTa[,(inherits(class_Vars,"numeric")|inherits(class_Vars,"integer"))]
+  # only numeric
+  Namesnum <- names(daTa)
+  daTa <- data[, c(Namesnum, Y) ]# new data with numeric + response
+  PP <- list()
+  I <- 1
+  for (i in Namesnum)
+  {
+    GG1 <-  daTa |> ggplot(aes(x = .data[[i]], .data[[Y]]))+geom_point()+
+      ggtitle("no trans")
+    PP[[I]] <- GG1
+    I <- I + 1
+    #  cat(I,"\n")
+    GG2 <-  daTa |> ggplot(aes(x = sqrt(.data[[i]]), .data[[Y]]))+
+      geom_point()+ggtitle("sqrt")
+    PP[[I]] <- GG2
+    I <- I + 1
+    #         cat(I,"\n")
+    GG3 <-  daTa |> ggplot(aes(x = log(.data[[i]]), .data[[Y]]))+
+      geom_point()+ggtitle("log")
+    PP[[I]] <- GG3
+    I <- I + 1
+    #           cat(I,"\n")
+  }
+  n.plots <- length(PP)
+  if (one.by.one)
+  {
+    oask <- devAskNewPage(one.by.one)
+    on.exit(devAskNewPage(oask))
+    for (i in 1:n.plots) print(PP[[i]])
+  }
+  else
+  { # multiple plots
+    #################################################################
+    define_region <- function(row, col){
+      viewport(layout.pos.row=row, layout.pos.col=col) }
+    #################################################################
+    if (n.plots>plots.per.page)
+    {
+      pages <- ceiling(n.plots/plots.per.page)
+      page <- n.plots%/%plots.per.page
+      ppp <- rep(plots.per.page,page)
+      if (n.plots%%plots.per.page != 0) ppp <- c(ppp, n.plots%%plots.per.page)
+      if (plots.per.page==9)
+      {
+        nc <- 3
+        nr <- 3
+        IJ <- expand.grid(j=1:nc, i=1:nr)
+      } else
+      {
+        if (is.null(nrow)||is.null(nrow)) stop("the nrow and ncol need to be defined")
+        if (plots.per.page> ncol*nrow) stop("the nrow or ncol has to increase")
+        nc <- ncol
+        nr <- nrow
+        IJ <- expand.grid(j=1:nc, i=1:nr)
+        IJ <- IJ[1:plots.per.page,]
+      }
+      start <- 1
+      finish <- ppp[1]
+      for (pa in 1:pages)
+      {
+        grid.newpage()
+        pushViewport(viewport(layout=grid.layout(nrow=nr,ncol=nc)))
+        for (p  in start:finish)
+        {
+          print(PP[[p]], vp=define_region(IJ$i[p],IJ$j[p]))
+        }
+        start <- finish +1
+        finish <- finish+ppp[pa+1]
+        IJ <- rbind(IJ, IJ)
+        oask <- devAskNewPage(ask=TRUE)
+        on.exit(devAskNewPage(oask))
+      }
+    } else
+    {
+      pages <- 1
+      ppp <- n.plots%/%pages
+      nc  <- nr <- trunc(sqrt(ppp))
+      if (nc < 1)        nr <- nc <- 1
+      if (nc * nr < ppp) nc <- nc + 1
+      if (nc * nr < ppp) nr <- nr + 1
+      IJ <- expand.grid(j=1:nc, i=1:nr)
+      grid.newpage()
+      pushViewport(viewport(layout=grid.layout(nrow=nr,ncol=nc)))
+      for (p  in 1:n.plots)
+      {
+        print(PP[[p]], vp=define_region(IJ$i[p],IJ$j[p]))
+      }
+    }
+  }
+  on.exit( pushViewport(viewport(layout=grid.layout(nrow=1,ncol=1))))
+  invisible(PP)
+}
+################################################################################
+################################################################################
+################################################################################
+################################################################################
+# function 3 #not finished yet
+data_Ptrans <- function(data, response,
+                    lim.trans = c(0, 1.5), 
+                         prof = FALSE,
+                            k = 2,
+                            ...)
+{
+  daTa <- mgcv <- NULL  
+# what is the data
+if (is(data, "list"))  stop("the data is list  the function needs a data.frame")
+if (is(data, "table")) stop("the data is a table the function needs a data.frame")
+if (is(data, "matrix")) data <- as.data.frame(data)
+if (is(data[1],"mts"))  data <- as.data.frame(data)
+if (is(data, "array")) stop("the data is an array the function needs a
+                            data.frame")
+     dimD <- dim(data)
+# checking data  
+if (is.null(dimD)) stop("only one variable in the data") 
+if (dimD[1]<20)   stop(cat("the size of the data set is too small", "\n",
+                             "to detect non-linear correlations", "\n"))  
+     Y <-  deparse(substitute(response))
+if (any(!(Y %in%names(data)))) stop("the response should be in data")
+     browser()
+     dat <- data_only_continuous(data)   
+   #  da <- subset(dat, -Y)
+#   sat.cont <- sapply(data,is.factor)|sapply(data,is.character)|
+# data_distinct(data, get.distinct=TRUE) < min.distinct|
+#     sapply(data, function(x) is(x, class2="Date"))
+#       daTa <- subset(data,  select=!sat.cont)  
+       Dim <- dim(dat)
+if (Dim[2]==0) stop("no variable is left after taking out the factors")         
+if (Dim[2]==1) stop("only one variable is left after taking out the factors")   
+   Names <- names(dat)
+class_Vars <- sapply(daTa,function(x) class(x)[1]) 
+        PP <- list()
+for (i in 1:length(class_Vars))
+  { 
+  
+    PP[[i]] <-  xy_Ptrans(daTa[,i],  lim.trans = c(0, 1.5), prof=F)
+  }
+  names(PP) <- Names       
+  PP 
+}
+################################################################################
+################################################################################
+################################################################################
+################################################################################
+# function 16 for time series
+y_time2num <- function(time, pattern=":")
+{
+t <-  gsub(pattern, ".", time)
+as.numeric(t)
+}
+################################################################################
+################################################################################
+################################################################################
+################################################################################
+# END of TIME functions
